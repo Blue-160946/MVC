c52eb007398d997b7ca3edb57f9d763db9750206
Blue-160946 <blueblue160946@gmail.com>
2025-01-24 11:12:53 +0700

    Edit

--- Main.java
+++ Main.java
@@ -2,14 +2,14 @@ import model.Student_Model;
 import view.Student_View;
 import controller.Student_Controller;
 
-public class Main { // ต้องมีคลาสสำหรับ main method
+public class Main {
     public static void main(String[] args) {
-        // สร้างออบเจกต์สำหรับ Model, View และ Controller
+        
         Student_Model model = new Student_Model();
         Student_View view = new Student_View();
         Student_Controller controller = new Student_Controller(model, view);
 
-        // เรียกใช้ Controller เพื่อรันโปรแกรม
+        
         controller.run();
     }
 }
